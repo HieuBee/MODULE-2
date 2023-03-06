0a9cf63b23b9f90bb1fb5b6af27ce76a69479184
HieuBee <minhhieuphanvan0126@gmail.com>
2023-03-06 19:18:29 +0700

    Sort Algorithms

---
+++ SortAlgorithms/src/illustrated_sorting_algorithm/Manager.java
@@ -0,0 +1,39 @@
+package illustrated_sorting_algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Manager {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Input size of array: ");
+        int size = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Input item " + i + " of array:");
+            numbers[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.println("Before array: " + Arrays.toString(numbers));
+        System.out.println("After array: " + Arrays.toString(insertionSort(numbers)));
+    }
+
+    public static int[] insertionSort(int[] numArr) {
+        int len = numArr.length;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j > 0; j--) {
+                if (numArr[j] < numArr[j - 1]) {
+                    int temp = numArr[j - 1];
+                    numArr[j - 1] = numArr[j];
+                    numArr[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+        return numArr;
+    }
+}
